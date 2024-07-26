38a0122e5191c1d74def3eedb8dfc82381586467
AlexR055 <166668608+AlexR055@users.noreply.github.com>
2024-07-26 13:41:45 -0700

    Update SGMath.py

--- SGMath.py
+++ SGMath.py
@@ -1,6 +1,6 @@
 import random
 import sys
-import numpy
+
 from math import *
 from SGadditionaldevices import main
 
@@ -13,7 +13,7 @@ additangle2 = 0
 # Enters number of particles
 while True:
     try:
-        particles = int(input("How many random spin 1/2 particles?: ")).strip().lower()
+        particles = int(input("How many random spin 1/2 particles?: "))
 
         if particles > 100000:
             print("Too many random spin 1/2 particles! Must be 100000 or less.")
@@ -21,7 +21,7 @@ while True:
         else:
             while True:
                 try:
-                    angle = int(input("What is the angle in degrees?: ")).strip()
+                    angle = int(input("What is the angle in degrees?: "))
                     break
                 except ValueError:
                     pass
@@ -35,8 +35,8 @@ while True:
 main()
 
 radangle = angle * pi / 180  # Converts angle to radians
-upper = (particles*((cos(radangle/2))**2))
-lower = (particles*((sin(radangle/2))**2))
+upper = (particles/2)
+lower = (particles/2)
 # Creates the random element for particle distribution
 randupper = random.randint((round(upper)-random.randint(1,100)),round(upper)+random.randint(1,100))
 randlower = particles-randupper
@@ -53,6 +53,7 @@ if devnumber == 1:
     radangle1 = additangle1*pi/180
     upper1 = randupper*(cos(radangle1/2)**2)
     lower1 = randlower*(sin(radangle1/2)**2)
+    print()
     print(
         f"Upper channel: {round(upper1)}, Lower channel: {round(lower1)}, Total: {round(upper1 + lower1)}")
 
@@ -64,13 +65,31 @@ elif devnumber == 2:
     lower1 = randlower * (sin(radangle1 / 2) ** 2)
     upper2 = upper1 * (cos(radangle2 / 2))**2
     lower2 = lower1 * (sin(radangle2 / 2))**2
+    print()
     print(
         f"Upper channel: {round(upper2)}, Lower channel: {round(lower2)}, Total: {round(upper2 + lower2)}")
 
 # No additional devices
 elif devnumber == 0:
+    print()
     print(
         f"Upper channel: {round(randupper)}, Lower channel: {round(randlower)}, Total: {round(randupper + randlower)}")
+ratioupper = round(randupper)/particles
+ratiolower = round(randlower)/particles
+
+print()
+print(f"Upper to Lower Ratio: {ratioupper}:{ratiolower}")
+
+print()
+print("Notice how the split between the upper and lower channels is nearly 50/50, "
+      "no matter the successive angled devices?")
+
+
+
+
+
+
+
 
 
 
